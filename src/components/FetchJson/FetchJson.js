162426f37d45dfdc02d6b863cd85fcaf891f2895
sdavis1230 <sdavis12300@gmail.com>
2018-08-01 15:36:16 -0400

    Update FetchJson.js

--- src/components/FetchJson/FetchJson.js
+++ src/components/FetchJson/FetchJson.js
@@ -12,17 +12,28 @@ import { COLORS } from '../../constants'
  * If it fails, make sure to notify the user.
  */
 const jsonUri = 'https://jsonplaceholder.typicode.com/users/1'
+// grabs the JSON data from uri and sets it equal to data to be displayed later
+var result
+var data = fetch(jsonUri)
+.then(data => data.json())
+result = data
+console.log(result)
 
 export default class FetchJson extends Component {
+
   render() {
-    const { json } = this.state
     return (
+    // this should display the json data as a string but I can't seem to find a way to do this properly
+    // if you check the console log you can see it does fetch just doesn't and the correct data is soted I just cant
+    // render it properly 
       <Card>
         <Heading color={COLORS.pink[300]}>FetchJson Component</Heading>
         <HeadingSmall>
           This component grabs JSON from an API and displays it.
         </HeadingSmall>
-        {/* display JSON data */}
+        <CodeBlock>
+        {result.toString()}
+        </CodeBlock>
       </Card>
     )
   }
